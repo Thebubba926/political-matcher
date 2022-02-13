b385819c5241e9ae27e494e57951a8ed77ada910
Daniel Bohen <bohen@usc.edu>
2022-02-13 12:33:00 -0800

    second commit

--- politicalMatcher/surveys/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from . import models
-
-class SurveysAdmin(admin.ModelAdmin):
-    list_display = ('title',)
-
-admin.site.register(models.Surveys, SurveysAdmin)
