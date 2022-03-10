de95ebd0ce21b69bed76c35830178d0432b0b2ea
Daniel Bohen <bohen@usc.edu>
2022-03-09 17:08:08 -0800

    hosts4

--- smartnotes/settings.py
+++ smartnotes/settings.py
@@ -127,9 +127,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/staticfiles/'
-STATICFILES_DIRS = [ 
-    BASE_DIR / 'staticfiles',
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
