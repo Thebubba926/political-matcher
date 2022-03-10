56bd35a1ae459dc0bbfddfe54a970f4493fe60ab
Daniel Bohen <bohen@usc.edu>
2022-03-09 16:59:35 -0800

    hosts

--- smartnotes/settings.py
+++ smartnotes/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-_ld3+jqx@(ng#de!esw7u2_=645&*$zn29pi2ti_4$evh=^f_j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://political-matcher.herokuapp.com/',
+'http://political-matcher.herokuapp.com/',
+'political-matcher.herokuapp.com/',
+'political-matcher.herokuapp.com/']
 
 
 # Application definition
