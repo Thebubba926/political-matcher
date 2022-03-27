eb98f45d67b07af0a43667104a746e2234604c9d
Daniel Bohen <bohen@usc.edu>
2022-03-27 12:18:45 -0700

    Update models.py

--- notes/models.py
+++ notes/models.py
@@ -5,5 +5,6 @@ class Notes(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     guns = models.CharField(max_length = 100)
+    knives = models.CharField(max_length = 100)
     created = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
