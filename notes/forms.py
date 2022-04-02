2da4157ac0064b80d69373eb6c0d9eaf30a278a2
Thebubba926 <102484960+Thebubba926@users.noreply.github.com>
2022-04-02 16:27:44 -0700

    Update forms.py
    Not 100% about what I am supposed to change in the models.py file.
    
    Also bc I don't have access these will come across as suggestions so I hope that you can just click like an "ok" button and it will go through. 
    I will continue to work on this tonight.

--- notes/forms.py
+++ notes/forms.py
@@ -18,12 +18,22 @@ class NotesForm(forms.ModelForm, forms.Form):
             'title': forms.TextInput(attrs={'class': 'form-control my-5'}),
             'text': forms.Textarea(attrs={"class": "form-control mb-5"}),
             'guns': forms.RadioSelect(choices=position_strength),
-            'knives': forms.RadioSelect(choices=position_strength)
+            'knives': forms.RadioSelect(choices=position_strength),
+            'Abortion': forms.RadioSelect(choices=position_strength),
+            'MarijuanaMed': forms.RadioSelect(choices=position_strength),
+            'MarijuanaRec': forms.RadioSelect(choices=position_strength),
+            'VoterFraud': forms.RadioSelect(choices=position_strength),
+            'WealthyTax': forms.RadioSelect(choices=position_strength)
         }
         labels = {
             'text': 'Write your thoughts here:',
             'guns': 'Do you support gun control',
-            'knives': 'Do you support knife control'
+            'knives': 'Do you support knife control',
+            'Abortion': 'Abortion should be illegal under any circumstace',
+            'MarijuanaMed': 'Marijuana should be legalized for Medical use',
+            'MarijuanaRec': 'Marijuana should be legalized for recreational use',
+            'VoterFraud': 'Voter fraud is a serious issue in our electoral system',
+            'WealthyTax': 'Wealthy individuals are taxed excessively'
         }
 
 
